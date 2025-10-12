73497a515bcd2609ea3ac9ee188f851e385a7bc4
Akshay Gawai <akshayg@Akshays-MacBook-Air.local>
2025-10-12 21:37:11 +0530

    Basic Model Added

--- Tweet/models.py
+++ Tweet/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.first_name + "_" + self.last_name
+    
